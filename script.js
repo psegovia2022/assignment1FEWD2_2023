243ee94042d2b28f841a98f7cf09b371249b9860
Paulina Segovia C <paulinasegovia@gmail.com>
2023-02-16 12:52:48 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+// STEP 1
+
+// let textCount = 'I like to eat apples when it is sunny and not raining.'
+
+// console.log(`The sentence has ${textCount.length} characters`);
+
+// STEP 2
+
+// let userName = prompt('What is your name');
+// let userAge = parseInt(prompt('How old are you?'));
+
+// document.body.innerHTML = `<h1> Your name is ${userName} and you are ${userAge} years old.`
+
+
+// STEP 3
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."';
+
+// log(someString);
+
+// It is not ValidityState. The following is ONE of the possibilities to make it work.
+
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+
+// console.log(someString);
+
+// STEP 4
+
+// let userName = prompt('What is your name');
+
+// STEP 5
+
+//  function squareNum(num) {
+//     return num * num;
+// }
+// console.log(squareNum(3));
+
+// STEP 6
+
+// let sum = function addNumbers(num1, num2) {
+//     let result = num1 + num2;
+//     console.log('The sum of ' + num1 + ' and ' + num2 + ' is ' + result);
+// };
+// sum (2, 3)
